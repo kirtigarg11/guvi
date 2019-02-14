a2f00a56addc906ff80408de6bf4a83357e07da8
kirtigarg11 <kirti.garg_cs16@gla.ac.in>
2019-02-15 01:26:36 +0530

    error fixed

--- capatilizefirstch.py
+++ capatilizefirstch.py
@@ -2,16 +2,18 @@ s=input()
 l=s.split(" ")
 
 for i in range(len(l)):
-	s=''
-	s1=''
-	s=l[i]
+        if((l[i].isnumeric())!=True):
+           
+                s=''
+                s1=''
+                s=l[i]
   
-	s1=l[i]
-	s=s[0]
+                s1=l[i]
+                s=s[0]
   
-	s=chr(ord(s)-32)
-	s=s+s1[1:]
-	l[i]=s
+                s=chr(ord(s)-32)
+                s=s+s1[1:]
+                l[i]=s
 k=''
 k=" ".join(l)
 print(k)
